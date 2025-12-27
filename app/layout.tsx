0a646fcd929185fabed3f4b558e6c264e69eec32
Gavin Lu <lu.gavinn@gmail.com>
2025-12-27 16:50:31 +1100

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        <main className="relative overflow-hidden">
+       
           {children}
-        </main>
+       
         <Footer />
       </body>
     </html>
