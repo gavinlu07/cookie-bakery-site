0961d651c4a012129c090ffc164c6a59d5e3d3e8
Gavin Lu <lu.gavinn@gmail.com>
2025-12-27 14:51:22 +1100

    Navbar mobile functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { FiMenu } from "react-icons/fi";
+import { AiOutlineMenu, AiOutlineClose, AiOutlineInstagram, AiOutlineFacebook, AiOutlineYoutube } from "react-icons/ai";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -17,7 +17,63 @@ const Navbar = () => {
       <div className="flex justify-center items-center h-full px-4 2xl:px-16">
         <div className="flex flex-1">
           <div onClick={handleNav} className="sm:hidden cursor-pointer pl-5 pr-5">
-            <FiMenu size={30}/>
+            <AiOutlineMenu size={30}/>
+          </div>
+          <div className={
+            menuOpen
+            ? "fixed left-0 top-0 top-0 w-[65%] sm:hidden h-screen bg-amber-50 ease-in duration-500"
+            : "fixed left-[-100%] h-screen top-0 ease-in duration-500"
+            }>
+            <div className="flex w-full items-center justify-end pr-10 pt-10">
+              <div onClick={handleNav} className="sm:hidden cursor-pointer">
+                <AiOutlineClose size={30}/>
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-center pt-5">
+              <Image
+                src="/bakery-logo-template.svg"
+                alt="Logo"
+                width="100"
+                height="100"
+                className=""
+                priority
+              />
+            </div>
+            <div className="flex-col py-4">
+              <ul>
+                <Link href="/">
+                  <li onClick={() => setMenuOpen(false)} className="py-4 p-15 cursor-pointer hover:bg-amber-100">
+                    Order
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li onClick={() => setMenuOpen(false)} className="py-4 p-15 cursor-pointer hover:bg-amber-100">
+                    Location
+                  </li>
+                </Link>
+                                <Link href="/">
+                  <li onClick={() => setMenuOpen(false)} className="py-4 p-15 cursor-pointer hover:bg-amber-100">
+                    Our Story
+                  </li>
+                </Link>
+                <Link href="/">
+                  <li onClick={() => setMenuOpen(false)} className="py-4 p-15 cursor-pointer hover:bg-amber-100">
+                    Contact Us
+                  </li>
+                </Link>
+              </ul>
+            </div>
+            <div className="flex flex-row justify-around pt-5 items-center pl-15 pr-15">
+              <Link href="/">
+                <AiOutlineInstagram size={(35)} className="cursor-pointer"/>
+              </Link>
+              <Link href="/">
+                <AiOutlineFacebook size={(35)} className="cursor-pointer"/>
+              </Link>
+              <Link href="/">
+                <AiOutlineYoutube size={(35)} className="cursor-pointer"/>
+              </Link>
+            </div>
           </div>
           <div className="flex flex-1 justify-end items-center">
             <ul className="hidden sm:flex">
