d94d82101794d354432e611da667b4bf0419297a
Gavin Lu <lu.gavinn@gmail.com>
2026-01-02 19:50:43 +1100

    minor imprpovments
    - Link added to order section
    - hero text black

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
     <section className='pt-24 pb-5 bg-amber-50'>
         <div className="flex flex-col justify-center items-center text-center lg:flex-row">            
             <div className="justify-center items-center text-center pt-5 lg:pt-0 ml-20 mr-20 lg:ml-10 lg:mr-20 lg:max-w-125 lg:text-left md:max-w-150"> 
-                <h1 className={`${caveatBrush.className} text-6xl md:text-7xl lg:text-8xl`}>Try Canberra's newest cookies</h1>
+                <h1 className={`${caveatBrush.className} text-6xl md:text-7xl lg:text-8xl text-black`}>Try Canberra's newest cookies</h1>
                 
                 <button type="button" 
                 title="order now" 
--- app/components/Order.tsx
+++ app/components/Order.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Image from 'next/image'
 import { Caveat_Brush } from "next/font/google";
 import { Changa_One } from "next/font/google";
+import Link from "next/link";
 
 
 const changaOne = Changa_One({
@@ -24,58 +25,61 @@ const Order = () => {
         <p className={`${changaOne.className} text-4xl md:text-5xl lg:text-5xl text-white pt-5`}>Build Your Box</p>
       </div>
       <div className='flex flex-col md:flex-row items-center justify-center lg:pt-5'>
-        <div className='border-0 hover:bg-[#CB2985] rounded-2xl w-max p-2.5 m-2.5'>
-          <div className='flex items-center justify-center'>
-            <Image
-            src="/6-pack-image.png"
-            alt="Logo"
-            width="400"
-            height="200"
-            className="rounded-2xl border-2 border-white"
-            priority
-            />
-          </div>
-          <div className='flex flex-row pt-2 pb-2'>
-            <div className='w-70'>
-              <p className={`${caveatBrush.className} text-left text-4xl md:text-4xl lg:text-4xl text-white`}>6-Pack Cookies</p>
-            </div>
-            <div className='flex w-30 justify-end text-center'>
-              <p className={`${changaOne.className} text-3xl md:text-4xl lg:text-4xl text-white`}>$50.00</p>
-            </div>
-          </div>
-          <hr className='border-2 rounded-2x1 text-white'></hr>
-          <div>
-            <p className={`w-100 text-1xl text-white pt-2`}>Try every cookie in our 6-Pack — perfect for sharing, gifting, or treating yourself.</p>
-          </div>
-        </div>
         
-        <div className='border-0 w-max hover:bg-[#CB2985] rounded-2xl m-2.5 p-2.5'>
-          <div className='flex items-center justify-center'>
-            <Image
-            src="/12-pack-image.png"
-            alt="Logo"
-            width="400"
-            height="200"
-            className="rounded-2xl border-2 border-white"
-            priority
-            />
-          </div>
-          <div className='flex flex-row pt-2 pb-2'>
-            <div className='w-70'>
-              <p className={`${caveatBrush.className} text-left text-4xl md:text-4xl lg:text-4xl text-white`}>12-Pack Cookies</p>
+        <Link href="/">
+          <div className='border-0 hover:bg-[#CB2985] rounded-2xl cursor-pointer w-max p-2.5 m-2.5'>
+            <div className='flex items-center justify-center'>
+              <Image
+              src="/6-pack-image.png"
+              alt="Logo"
+              width="400"
+              height="200"
+              className="rounded-2xl border-2 border-white"
+              priority
+              />
             </div>
-            <div className='flex w-30 justify-end text-center'>
-              <p className={`${changaOne.className} text-3xl md:text-4xl lg:text-4xl text-white`}>$95.00</p>
+            <div className='flex flex-row pt-2 pb-2'>
+              <div className='w-70'>
+                <p className={`${caveatBrush.className} text-left text-4xl md:text-4xl lg:text-4xl text-white`}>6-Pack Cookies</p>
+              </div>
+              <div className='flex w-30 justify-end text-center'>
+                <p className={`${changaOne.className} text-3xl md:text-4xl lg:text-4xl text-white`}>$50.00</p>
+              </div>
+            </div>
+            <hr className='border-2 rounded-2x1 text-white'></hr>
+            <div>
+              <p className={`w-100 text-1xl text-white pt-2`}>Try every cookie in our 6-Pack — perfect for sharing, gifting, or treating yourself.</p>
             </div>
           </div>
-          <hr className='border-2 rounded-2x1 text-white'></hr>
-          <div>
-            <p className={`w-100 text-1xl text-white pt-2`}>Get the best value with our 12-Pack, ideal for sharing at parties, events, or catering.</p>
+        </Link>
+
+        <Link href="/">
+          <div className='border-0 w-max hover:bg-[#CB2985] rounded-2xl cursor-pointer m-2.5 p-2.5'>
+            <div className='flex items-center justify-center'>
+              <Image
+              src="/12-pack-image.png"
+              alt="Logo"
+              width="400"
+              height="200"
+              className="rounded-2xl border-2 border-white"
+              priority
+              />
+            </div>
+            <div className='flex flex-row pt-2 pb-2'>
+              <div className='w-70'>
+                <p className={`${caveatBrush.className} text-left text-4xl md:text-4xl lg:text-4xl text-white`}>12-Pack Cookies</p>
+              </div>
+              <div className='flex w-30 justify-end text-center'>
+                <p className={`${changaOne.className} text-3xl md:text-4xl lg:text-4xl text-white`}>$95.00</p>
+              </div>
+            </div>
+            <hr className='border-2 rounded-2x1 text-white'></hr>
+            <div>
+              <p className={`w-100 text-1xl text-white pt-2`}>Get the best value with our 12-Pack, ideal for sharing at parties, events, or catering.</p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
-
-
     </section>
   )
 }
